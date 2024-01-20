73ddc0ac180533e6bbe88814d809c1dd253f91d6
yooooka <yooooka@gmail.com>
2024-01-20 15:11:25 -0800

    tailwind nesting setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ export default defineConfig({
   // ...
   integrations: [
     tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: true,
+      // Example: Allow writing nested CSS declarations
+      // alongside Tailwind's syntax
+      nesting: true,
     }),
   ],
 });
